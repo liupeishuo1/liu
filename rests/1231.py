00e6e5fc196dd5df47aacc549ac4761546cbc779
刘培硕 <1493454571@qq.com>
2021-09-18 14:30:03 +0800

    first commit

---
+++ rests/1231.py
@@ -0,0 +1,18 @@
+
+from MyQR import myqr
+myqr.run(
+    words='http://49.232.204.110/',
+
+    version=5,
+
+    level='H',
+
+    picture="C:/edge下载/22222.gif",
+
+    colorized=True,
+
+    contrast=1.0,
+
+    brightness=1.0,
+
+    save_name='Py.gif',)
---
+++ rests/网址图片二维码.py
@@ -0,0 +1,7 @@
+from MyQR import myqr
+myqr.run(
+  words='http://49.232.204.110/',
+  picture='D:/桌面/22222.gif',
+  colorized=True,
+  save_name='bab.gif'
+)
